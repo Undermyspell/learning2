d98d40e966b1e22e77fea1a249caabfe88942305
Michael Kaiser <michael.kaiser@conplement.de>
2020-05-05 12:17:52 +0200

    adds test photos

--- nodejs-api/index.js
+++ nodejs-api/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/test", (req, res, next) => {
-    res.json({name: "test"});
+app.get("/imageurls", (req, res, next) => {
+    const filenames = [];
+    const directoryPath = path.join(__dirname, 'images');
+    fs.readdir(directoryPath, function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        files.forEach(function (file) {
+            filenames.push(file);
+        });
+
+        res.json(filenames);
+    });
 });
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
