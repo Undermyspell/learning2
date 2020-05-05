8468802b9b44d5bff567f18a109ba18da48e8471
Michael Kaiser <michael.kaiser@conplement.de>
2020-05-05 12:17:25 +0200

    adds image gallery and styling

--- angular-app/angular/src/app/app.module.ts
+++ angular-app/angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './home/nav/nav.component';
 import { ContainerComponent } from './gallery/container/container.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ContainerComponent } from './gallery/container/container.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
