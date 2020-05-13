e05cfa9aa575bab5e2f25b82ede3cc310a7a2293
Undermyspell <michael_kaiser2@outlook.com>
2020-05-13 16:06:50 +0200

    adds dotenv

--- nodejs-api/index.js
+++ nodejs-api/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
+
 
 const app = express();
 app.use(cors());
