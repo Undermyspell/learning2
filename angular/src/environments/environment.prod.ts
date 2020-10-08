5949302dc5d0602dbea811140a4d75158dda4d77
Michael Kaiser <michael.kaiser@conplement.de>
2020-10-08 13:39:12 +0200

    adds firebase conf for prod

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyAc-aHXDV3cLl490GQeam80UgOpT4rteIw",
+    authDomain: "mklearningcloudrun.firebaseapp.com",
+    databaseURL: "https://mklearningcloudrun.firebaseio.com",
+    projectId: "mklearningcloudrun",
+    storageBucket: "mklearningcloudrun.appspot.com",
+    messagingSenderId: "1038518562712",
+    appId: "1:1038518562712:web:4f770cf2b41f355b9f05ed"
+  }
 };
