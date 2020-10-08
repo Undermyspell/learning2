960357276adfbe678a954698538fe7d4562f19f2
Undermyspell <michael_kaiser2@outlook.com>
2020-10-08 20:43:50 +0200

    remove firebase cred

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-
-  firebase: {
-    apiKey: "AIzaSyAc-aHXDV3cLl490GQeam80UgOpT4rteIw",
-    authDomain: "mklearningcloudrun.firebaseapp.com",
-    databaseURL: "https://mklearningcloudrun.firebaseio.com",
-    projectId: "mklearningcloudrun",
-    storageBucket: "mklearningcloudrun.appspot.com",
-    messagingSenderId: "1038518562712",
-    appId: "1:1038518562712:web:4f770cf2b41f355b9f05ed"
-  }
+  production: true
 };
