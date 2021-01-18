cd291584bfd53d1d8b38e7c10674acff4c99cfd5
Michael Kaiser <michael.kaiser@conplement.de>
2021-01-18 07:41:47 +0100

    adds address from env variables

--- node-grpc/server/index.js
+++ node-grpc/server/index.js
@@ -17,9 +17,9 @@ const server = new grpc.Server();
 server.addService(sponsorsProto.SponsorsService.service, sponsorsService);
 
 const PORT = process.env.PORT || 30043;
-const ADRESS = "0.0.0.0";
+const ADDRESS = process.env.ADDRESS || "0.0.0.0";
 
-console.log(`Server listening on ${ADRESS}:${PORT}`)
+console.log(`Server listening on ${ADDRESS}:${PORT}`)
 server.bind(`${ADRESS}:${PORT}`, grpc.ServerCredentials.createInsecure());
 console.log("Server running at http://0.0.0.0:30043");
 server.start();
