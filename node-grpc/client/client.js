35bb92fe95d618fd918c4c2c733613d114ed82a2
Michael Kaiser <michael.kaiser@conplement.de>
2021-01-22 07:34:39 +0100

    revert ports

--- node-grpc/client/client.js
+++ node-grpc/client/client.js
@@ -10,8 +10,8 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     arrays: true
 });
 
-const GRPCENDPOINT = process.env.GRPCENDPOINT || "localhost";
-const PORT = process.env.PORT || 30043;
+const GRPCENDPOINT = process.env.GRPCENDPOINT || "mknode-grpc-server-jufkqxqefa-ew.a.run.app";
+const PORT = process.env.PORT || 443;
 const channelCredentials = PORT == 443 ? grpc.credentials.createSsl() : grpc.credentials.createInsecure();
 
 console.log(`gRPC endpoint: ${GRPCENDPOINT}`)
